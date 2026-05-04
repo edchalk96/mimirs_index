75870c4854f3df2fa5c971db375776f19090b091
edchalk96 <edchalk96@gmail.com>
2026-05-04 22:53:00 +0100

    Added a sort functionality to the edda library page

--- the_edda_library/urls.py
+++ the_edda_library/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.LibraryView.as_view(), name="library"),
+    path("", views.LoreList.as_view(), name="library"),
 ]
--- the_edda_library/views.py
+++ the_edda_library/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
 from django.views import generic
 from .models import Lore
 
 # Create your views here.
 class LoreList(generic.ListView):
-    queryset = Lore.objects.filter(status=1).order_by('-created_on')
+    model = Lore
     template_name = "the_edda_library/the_edda_library.html"
     paginate_by = 6
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        sort_by = self.request.GET.get("sort", "newest")
+
+        if sort_by == "az":
+            return queryset.order_by("title")
+        elif sort_by == "za":
+            return queryset.order_by("-title")
+        elif sort_by == "oldest":
+            return queryset.order_by("created_on")
+        else:  # Default to newest
+            return queryset.order_by("-created_on")
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_sort"] = self.request.GET.get("sort", "newest")
+        return context
