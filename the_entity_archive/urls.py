8ffda13d838bc85917f8bc50d9bb2939665cba15
edchalk96 <edchalk96@gmail.com>
2026-05-05 15:11:42 +0100

    Added in html template for entity profile page and relevant view function

--- the_entity_archive/urls.py
+++ the_entity_archive/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.EntityList.as_view(), name="archive"),
+    path("<name:name/", views.entity_Detail, name="entity_detail"),
 ]
--- the_entity_archive/views.py
+++ the_entity_archive/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Entity
 
@@ -20,3 +21,22 @@ class EntityList(generic.ListView):
         context = super().get_context_data(**kwargs)
         context["search_term"] = self.request.GET.get("q", "")
         return context
+    
+def entity_profile(request, name):
+    """
+    View function to display the profile of a specific entity.
+
+    **Context:**
+
+    `entity`
+        An instance of the Entity model that matches the provided name.
+
+    **Template**
+
+    :template:`the_entity_archive/entity_profile.html`
+
+    """
+    queryset = Entity.objects.filter(status=1)
+    entity = get_object_or_404(queryset, name=name)
+
+    return render(request, "the_entity_archive/entity_profile.html", {"entity": entity})
