5d85ecaf5c7d7371093123ecbd719e7d688f8c9c
edchalk96 <edchalk96@gmail.com>
2026-05-05 14:23:03 +0100

    Added in html template for lore detail and added the partial directory and comment thread html

--- the_edda_library/urls.py
+++ the_edda_library/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.LoreList.as_view(), name="library"),
+    path("<slug:slug>/", views.lore_detail, name="lore_detail"),
 ]
--- the_edda_library/views.py
+++ the_edda_library/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Lore
+from .models import Lore, Comment
 
 # Create your views here.
 class LoreList(generic.ListView):
@@ -8,6 +9,19 @@ class LoreList(generic.ListView):
     paginate_by = 6
 
     def get_queryset(self):
+        """
+        Override the default queryset to allow sorting based on query parameters.
+
+        **Context:**
+
+        `sort_by`
+            A string obtained from the query parameters that determines the sorting order.
+
+        **Template**
+
+        :template:`the_edda_library/the_edda_library.html`
+        
+        """
         queryset = super().get_queryset()
         sort_by = self.request.GET.get("sort", "newest")
 
@@ -21,6 +35,38 @@ class LoreList(generic.ListView):
             return queryset.order_by("-created_on")
         
     def get_context_data(self, **kwargs):
+        """
+        Override the default context data to include the current sort parameter.
+
+        **Context:**
+
+        `current_sort`
+            A string indicating the current sorting order.
+
+        **Template**
+
+        :template:`the_edda_library/the_edda_library.html`
+
+        """
         context = super().get_context_data(**kwargs)
         context["current_sort"] = self.request.GET.get("sort", "newest")
         return context
+    
+def lore_detail(request, slug):
+    """
+    View function to display the details of a specific lore entry.
+
+    **Context:**
+
+    `lore`
+        An instance of the Lore model corresponding to the provided slug.
+
+    **Template**
+
+    :template:`the_edda_library/lore_detail.html`
+
+    """
+    queryset = Lore.objects.filter(status=1)
+    lore = get_object_or_404(queryset, slug=slug)
+
+    return render(request, "the_edda_library/lore_detail.html", {"lore": lore})
