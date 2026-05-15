21f8d8d57d45d3e9c69327a2cd2700a98c537d9e
edchalk96 <edchalk96@gmail.com>
2026-05-15 15:42:24 +0100

    Bug fix for mimirs whisper section on the home page not working

--- home/views.py
+++ home/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views import generic
 from the_edda_library.models import Lore
 import random
@@ -8,12 +7,15 @@ class HomePageView(generic.TemplateView):
     template_name = "home/index.html"
 
 # Function to pull through a random Lore entry for the homepage - Credit to https://books.agiliq.com/projects/django-orm-cookbook/en/latest/random.html for this information on how to do this.
-def random_lore(request):
-    count = Lore.objects.count()
-    random_lore = None
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        approved_lore = Lore.objects.filter(status=1).prefetch_related('entities')
+        count = approved_lore.count()
+        random_lore = None
+        if count > 0:
+            random_index = random.randint(0, count - 1)
+            random_lore = approved_lore[random_index]
 
-    if count > 0:
-        random_index = random.randint(0, count - 1)
-        random_lore = Lore.objects.prefetch_related('entities').filter(status=1)[random_index]
+        context['random_lore'] = random_lore
 
-    return render(request, 'home/index.html', {'random_lore': random_lore})
+        return context
